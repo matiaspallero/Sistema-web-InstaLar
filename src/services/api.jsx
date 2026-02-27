0cb5af25a3e86acc4df339036fdfe377fe0ab444
Matias Pallero <matuutepallero@gmail.com>
2026-02-27 02:54:17 -0300

    para ver si anda en produ

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://instalar.netlify.app/api';
 
 const handleResponse = async (response) => {
   if (!response.ok) {
